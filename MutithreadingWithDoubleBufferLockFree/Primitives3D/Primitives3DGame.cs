2a29a65154e6fdd54a627f9ac5ea81bc954ccdff
Andrea <roundcrisis@gmail.com>
2013-05-25 19:49:18 +0100

    Adding a solution inspired by Quake 3 that just uses AutoResetEvents (no lock(object))

---
+++ MutithreadingWithDoubleBufferLockFree/Primitives3D/Primitives3DGame.cs
@@ -0,0 +1,192 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Primitives3D
+{
+	public class Primitives3DGame : Game
+	{
+		private const int NumPrimitiveObjects = 100;
+		GraphicsDeviceManager _graphics;
+
+		SpriteBatch _spriteBatch;
+		SpriteFont _spriteFont;
+
+		KeyboardState _currentKeyboardState;
+		KeyboardState _lastKeyboardState;
+		GamePadState _currentGamePadState;
+		GamePadState _lastGamePadState;
+		MouseState _currentMouseState;
+		MouseState _lastMouseState;
+
+		// Store a list of primitive models, plus which one is currently selected.
+		readonly List<GeometricPrimitive> _primitives = new List<GeometricPrimitive>();
+
+		int _currentPrimitiveIndex = 0;
+
+		// store a wireframe rasterize state
+		RasterizerState _wireFrameState;
+
+		// Store a list of tint colors, plus which one is currently selected.
+		List<Color> colors = new List<Color>
+        {
+            Color.Red,
+            Color.Green,
+            Color.Blue,
+            Color.White,
+            Color.Black,
+        };
+
+		int _currentColorIndex = 0;
+
+		// Are we rendering in wireframe mode?
+		bool _isWireframe;
+		private Task _threadedGameLoopTask;
+		private Renderer _renderer;
+		
+
+		public Primitives3DGame()
+		{
+			Content.RootDirectory = "Content";
+			_graphics = new GraphicsDeviceManager(this) { SynchronizeWithVerticalRetrace = false, PreferMultiSampling = true};
+			IsFixedTimeStep = false;
+			IsMouseVisible = true;
+		}
+		protected override void Initialize()
+		{
+			base.Initialize();
+			_renderer = new Renderer();
+			_threadedGameLoopTask = Task.Factory.StartNew(() =>
+			{
+				var gl = new AlternateGameLoop(_renderer);
+				gl.Loop();
+			});
+		}
+
+		protected override void LoadContent()
+		{
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+			_spriteFont = Content.Load<SpriteFont>("hudfont");
+
+			var random = new Random(Environment.TickCount);
+			for (int i = 0; i < NumPrimitiveObjects; i++)
+			{
+				var primitive = new CubePrimitive(GraphicsDevice)
+					{
+						Position = new Vector3(random.Next(100) - 50, random.Next(100) - 50, -random.Next(100))
+					};
+				_primitives.Add(primitive);
+			}
+
+			_wireFrameState = new RasterizerState
+			{
+				FillMode = FillMode.WireFrame,
+				CullMode = CullMode.None,
+			};
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			HandleInput();
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			if (_isWireframe)
+				GraphicsDevice.RasterizerState = _wireFrameState;
+			else
+				GraphicsDevice.RasterizerState = RasterizerState.CullCounterClockwise;
+			var cameraPosition = new Vector3(0, 0, 2.5f);
+			var aspect = GraphicsDevice.Viewport.AspectRatio;
+
+			var view = Matrix.CreateLookAt(cameraPosition, Vector3.Zero, Vector3.Up);
+			var projection = Matrix.CreatePerspectiveFieldOfView(1, aspect, 1, 1000);
+
+			_renderer.Draw(GraphicsDevice, view, projection);
+
+			// Reset the fill mode renderstate.
+			GraphicsDevice.RasterizerState = RasterizerState.CullCounterClockwise;
+
+			_spriteBatch.Begin();
+			Profiler.DrawStats(_spriteBatch, _spriteFont);
+			_spriteBatch.End();
+
+			Profiler.Clear();
+
+			base.Draw(gameTime);
+		}
+
+		void HandleInput()
+		{
+			_lastKeyboardState = _currentKeyboardState;
+			_lastGamePadState = _currentGamePadState;
+			_lastMouseState = _currentMouseState;
+
+			_currentKeyboardState = Keyboard.GetState();
+			_currentGamePadState = GamePad.GetState(PlayerIndex.One);
+			_currentMouseState = Mouse.GetState();
+
+			// Check for exit.
+			if (IsPressed(Keys.Escape, Buttons.Back))
+			{
+				Exit();
+			}
+
+			// Change primitive?
+			Viewport viewport = GraphicsDevice.Viewport;
+			int halfWidth = viewport.Width / 2;
+			int halfHeight = viewport.Height / 2;
+			var topOfScreen = new Rectangle(0, 0, viewport.Width, halfHeight);
+			if (IsPressed(Keys.A, Buttons.A) || LeftMouseIsPressed(topOfScreen))
+			{
+				_currentPrimitiveIndex = (_currentPrimitiveIndex + 1) % _primitives.Count;
+			}
+			
+			// Change color?
+			Rectangle botLeftOfScreen = new Rectangle(0, halfHeight, halfWidth, halfHeight);
+			if (IsPressed(Keys.B, Buttons.B) || LeftMouseIsPressed(botLeftOfScreen))
+			{
+				_currentColorIndex = (_currentColorIndex + 1) % colors.Count;
+			}
+
+
+			// Toggle wireframe?
+			Rectangle botRightOfScreen = new Rectangle(halfWidth, halfHeight, halfWidth, halfHeight);
+			if (IsPressed(Keys.Y, Buttons.Y) || LeftMouseIsPressed(botRightOfScreen))
+			{
+				_isWireframe = !_isWireframe;
+			}
+		}
+
+		bool IsPressed(Keys key, Buttons button)
+		{
+			return (_currentKeyboardState.IsKeyDown(key) && _lastKeyboardState.IsKeyUp(key)) ||
+				   (_currentGamePadState.IsButtonDown(button) && _lastGamePadState.IsButtonUp(button));
+		}
+
+		bool LeftMouseIsPressed(Rectangle rect)
+		{
+			return (_currentMouseState.LeftButton == ButtonState.Pressed &&
+					_lastMouseState.LeftButton != ButtonState.Pressed &&
+					rect.Contains(_currentMouseState.X, _currentMouseState.Y));
+		}
+	}
+
+	static class Program
+	{
+		static void Main()
+		{
+			using (var game = new Primitives3DGame())
+			{
+				game.Run();
+
+			}
+		}
+	}
+}
---
+++ MutithreadingWithDoubleBufferLockFree/Primitives3D/Renderer.cs
@@ -0,0 +1,92 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Primitives3D
+{
+	public class Renderer
+	{
+		private List<RenderCommand> _updatingRenderCommands;
+		private List<RenderCommand> _renderingRenderCommands;
+		private List<RenderCommand> _bufferedRenderCommandsA;
+		private List<RenderCommand> _bufferedRenderCommandsB;
+
+		private CubePrimitive _cubePrimitive;
+		private List<RenderCommand> _lastRenderCommands;
+
+		private AutoResetEvent _renderActive;
+		private AutoResetEvent _renderComandsReady;
+		private AutoResetEvent _renderCompleted;
+		private List<RenderCommand>[] _renderCommands;
+
+		public Renderer()
+		{
+			_lastRenderCommands = new List<RenderCommand>();
+
+			_renderCommands = new List<RenderCommand>[2];
+			_renderCommands[0]= new List<RenderCommand>();
+			_renderCommands[1]= new List<RenderCommand>();
+			
+			_bufferedRenderCommandsA = new List<RenderCommand>();
+			_bufferedRenderCommandsB = new List<RenderCommand>();
+			_updatingRenderCommands = _bufferedRenderCommandsA;
+
+			_renderComandsReady = new AutoResetEvent(false);
+
+			_renderActive = new AutoResetEvent(false);
+			_renderCompleted = new AutoResetEvent(true);
+		}
+
+		public void AddCube(Cube primitive)
+		{
+			var translation = Matrix.CreateFromYawPitchRoll(primitive.Rotation.X, primitive.Rotation.Y, primitive.Rotation.Z) *
+							  Matrix.CreateTranslation(primitive.Position);
+
+			_updatingRenderCommands.Add(new RenderCommand { Color = primitive.Color, Radius = primitive.Radius, World = translation });
+		}
+
+		public void EndFrame()
+		{
+			_renderCompleted.WaitOne();
+			_renderComandsReady.Set();
+			_renderActive.WaitOne();
+		}
+		
+		public void Draw(GraphicsDevice device, Matrix view, Matrix projection)
+		{
+			_renderCompleted.Reset();
+			SwapBuffers();
+			_renderActive.Set();
+
+			_cubePrimitive = _cubePrimitive ?? new CubePrimitive(device);
+			foreach (var renderingRenderCommand in _lastRenderCommands)
+			{
+				_cubePrimitive.Draw(renderingRenderCommand.World, view, projection, renderingRenderCommand.Color);
+			}
+			_renderActive.Reset();
+			_renderCompleted.Set();
+			_renderComandsReady.WaitOne();
+		}
+
+		private void SwapBuffers()
+		{
+			_renderComandsReady.Reset();
+			if (_updatingRenderCommands == _bufferedRenderCommandsA)
+			{
+				_updatingRenderCommands = _bufferedRenderCommandsB;
+				_renderingRenderCommands = _bufferedRenderCommandsA;
+
+			}
+			else if (_updatingRenderCommands == _bufferedRenderCommandsB)
+			{
+				_updatingRenderCommands = _bufferedRenderCommandsA;
+				_renderingRenderCommands = _bufferedRenderCommandsB;
+			}
+			_lastRenderCommands.Clear();
+			_lastRenderCommands.AddRange(_renderingRenderCommands);
+			_updatingRenderCommands.Clear();
+		}
+	}
+}
